abedb35b013f260c56ecf0973cbb663ecb749495
gowls <dnwpdnr123@naver.com>
2022-12-28 23:31:38 +0900

    button 추가

--- src/dynamic_beat/DynamicBeat.java
+++ src/dynamic_beat/DynamicBeat.java
@@ -8,30 +8,38 @@ public class DynamicBeat extends JFrame {
     // 배경 관련 필드
     private Image screenImage;
     private Graphics screenGraphic;
-    private Image introBackground;
-    private Image changeImg;
+    private Image introBackground = new ImageIcon(Main.class.getResource("../images/background.jpg")).getImage();;
+    private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menubar.png")));
+    private JButton exitButton = new JButton(new ImageIcon(Main.class.getResource("../images/exitButton.png")));
 
     public DynamicBeat(){
         setTitle("Dynamic Beat");
+        setUndecorated(true);
         setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        setBackground(new Color(0, 0, 0, 0));
+        setLayout(null);
 
-        introBackground = new ImageIcon(Main.class.getResource("../images/background.jpg")).getImage();
+        // 메뉴바 초기화
+        menuBar.setBounds(0, 0, 1280, 30);
+        add(menuBar);
+
+        // 닫기버튼 초기화
+        exitButton.setBounds(50, 50, 30, 30);
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setFocusPainted(false);
+        add(exitButton);
+
         // 이미지 크기를 스크린에 맞게 변환
         introBackground = introBackground.getScaledInstance(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT, Image.SCALE_SMOOTH);
 
         Music introMusic = new Music("introMusic.mp3", true);
         introMusic.start();
     }
-
-    // ImageIcon을 image로 변환하고 size를 조절한 뒤 다시 ImageIcon으로 변환
-    // 코드가 더 늘어나면 따로 클래스로 빼겠음..??
-//    public Image changeImageSize(Image icon, int width, int height){
-//
-//    }
 
     // 이미지 배경을 매순간마다 그려줌
     @Override
@@ -46,6 +54,7 @@ public class DynamicBeat extends JFrame {
 
     public void screenDraw(Graphics graphic){
         graphic.drawImage(introBackground, 0, 0, null);
+        paintComponents(graphic);
         this.repaint();
 
     }
